d63d73cafc4a0582ed53bb816b86c92f604429c3
Alex Chemus <aachemus@gmail.com>
2023-03-16 22:34:13 +0500

    uwu

--- src/widgets/post/components/index.ts
+++ src/widgets/post/components/index.ts
@@ -1,4 +1,3 @@
-export { default as PostAuthor } from './PostAuthor.vue'
 export { default as PostButtons } from './PostButtons.vue'
 export { default as PostComment } from './PostComment.vue'
 export { default as PostText } from './PostText.vue'
