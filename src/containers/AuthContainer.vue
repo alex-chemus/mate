cf8772ce3315697fd2fbe7d1e8e2d6c4790d1085
Alex Chemus <aachemus@gmail.com>
2023-06-18 14:18:08 +0500

    add path check to AuthContainer

--- src/containers/AuthContainer.vue
+++ src/containers/AuthContainer.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
+import { useRoute } from 'vue-router'
 import { onMounted } from 'vue'
 import { authActions } from '@/store/constants'
 import { useAuthState, useDispatch } from '@/utils'
 
 const dispatch = useDispatch()
 const state = useAuthState()
+const route = useRoute()
 
 onMounted(() => {
+  if (route.path === '/redirect') return
   dispatch(authActions.GET_LOCAL_TOKEN)
 })
 </script>
