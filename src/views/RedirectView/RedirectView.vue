f43918055e2e7529a0939c0e0850e5d85ddf8eba
Alex Chemus <aachemus@gmail.com>
2023-06-18 14:35:54 +0500

    add logger to RedirectView

--- src/views/RedirectView/RedirectView.vue
+++ src/views/RedirectView/RedirectView.vue
@@ -10,6 +10,7 @@ const commit = useCommit()
 const userState = useUserState()
 
 onMounted(() => {
+  console.log('route: ', route)
   if (route.query.token) {
     commit(authActions.SET_TOKEN, route.query.token)
   } else {
