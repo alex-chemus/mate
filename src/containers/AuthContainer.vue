06d3d0a620fcebcbb1b634c41125c124dd7ac080
Alex Chemus <aachemus@gmail.com>
2023-06-18 14:40:19 +0500

    update AuthContainer

--- src/containers/AuthContainer.vue
+++ src/containers/AuthContainer.vue
@@ -1,18 +1,20 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, computed } from 'vue'
 import { authActions } from '@/store/constants'
 import { useAuthState, useDispatch } from '@/utils'
 
 const dispatch = useDispatch()
 const state = useAuthState()
 
+const isRedirect = computed(() => window.location.pathname === '/redirect')
+
 onMounted(() => {
   alert(window.location.pathname)
-  if (window.location.pathname.startsWith('/redirect')) return
+  if (isRedirect.value) return
   dispatch(authActions.GET_LOCAL_TOKEN)
 })
 </script>
 
 <template>
-  <slot v-if="state.token" />
+  <slot v-if="state.token || isRedirect" />
 </template>
